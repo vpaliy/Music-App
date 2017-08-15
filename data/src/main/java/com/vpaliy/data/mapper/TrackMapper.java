4f988945900b3896ea93ac3f9aa9b66714bd427a
vpaliy <vpaliy97@gmail.com>
2017-08-14 23:54:27 -0400

    add track mapper

--- data/src/main/java/com/vpaliy/data/mapper/TrackMapper.java
+++ data/src/main/java/com/vpaliy/data/mapper/TrackMapper.java
@@ -1,5 +1,48 @@
 package com.vpaliy.data.mapper;
 
 
-public class TrackMapper {
+import com.vpaliy.domain.model.Track;
+import com.vpaliy.domain.model.User;
+import com.vpaliy.soundcloud.model.MiniUserEntity;
+import com.vpaliy.soundcloud.model.TrackEntity;
+
+public class TrackMapper extends Mapper<Track,TrackEntity> {
+
+    private Mapper<User,MiniUserEntity> userMapper;
+
+    public TrackMapper(Mapper<User,MiniUserEntity> userMapper){
+        this.userMapper=userMapper;
+    }
+
+    @Override
+    public Track map(TrackEntity trackEntity) {
+        if(trackEntity==null) return null;
+        Track track=new Track();
+        track.setId(trackEntity.id);
+        track.setStreamUrl(trackEntity.stream_url);
+        track.setUser(userMapper.map(trackEntity.user));
+        track.setArtworkUrl(trackEntity.artwork_url);
+        track.setDuration(trackEntity.duration);
+        track.setTags(MapperUtils.splitString(trackEntity.tags_list));
+        track.setReleaseDate(trackEntity.release);
+        track.setTitle(trackEntity.title);
+        //TODO fix this
+       // track.setArtist(trackEntity.ar);
+        return track;
+    }
+
+    @Override
+    public TrackEntity reverse(Track track) {
+        if(track==null) return null;
+        TrackEntity trackEntity=new TrackEntity();
+        trackEntity.id=track.getId();
+        trackEntity.stream_url=track.getStreamUrl();
+        trackEntity.user=userMapper.reverse(track.getUser());
+        trackEntity.artwork_url=track.getArtworkUrl();
+        trackEntity.duration=track.getDuration();
+        trackEntity.tags_list=MapperUtils.toString(track.getTags());
+        trackEntity.release=track.getReleaseDate();
+        trackEntity.title=track.getTitle();
+        return trackEntity;
+    }
 }
