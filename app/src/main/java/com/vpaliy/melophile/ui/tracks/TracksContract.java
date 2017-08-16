31b630b61433b6110e470cd6245fd325987c52c3
vpaliy <vpaliy97@gmail.com>
2017-08-16 12:51:06 -0400

    set up tracks screen

---
+++ app/src/main/java/com/vpaliy/melophile/ui/tracks/TracksContract.java
@@ -0,0 +1,4 @@
+package com.vpaliy.melophile.ui.tracks;
+
+public class TracksContract {
+}
---
+++ app/src/main/java/com/vpaliy/melophile/ui/tracks/TracksFragment.java
@@ -0,0 +1,8 @@
+package com.vpaliy.melophile.ui.tracks;
+
+/**
+ * Created by vpaliy on 16/08/17.
+ */
+
+public class TracksFragment {
+}
