028afdd522e2584066c6ce431c6186c5e494baef
vpaliy <vpaliy97@gmail.com>
2017-08-28 15:13:44 -0400

    data layer : : add uri matcher

--- data/src/main/java/com/vpaliy/data/source/local/MusicUriMatcher.java
+++ data/src/main/java/com/vpaliy/data/source/local/MusicUriMatcher.java
@@ -1,5 +1,48 @@
 package com.vpaliy.data.source.local;
 
+import android.content.UriMatcher;
+import android.net.Uri;
+import android.util.SparseArray;
 
 public class MusicUriMatcher {
+
+    private UriMatcher uriMatcher;
+    private SparseArray<MusicMatchEnum> codeMap;
+
+    public MusicUriMatcher(){
+        buildUriMatcher();
+        buildUriMap();
+    }
+
+    private void buildUriMatcher(){
+        uriMatcher=new UriMatcher(UriMatcher.NO_MATCH);
+        MusicMatchEnum[] enums=MusicMatchEnum.values();
+        for(MusicMatchEnum uriEnum:enums){
+            uriMatcher.addURI(MusicContract.CONTENT_AUTHORITY,uriEnum.path,uriEnum.code);
+        }
+    }
+
+    private void buildUriMap(){
+        codeMap=new SparseArray<>();
+        MusicMatchEnum[] enums=MusicMatchEnum.values();
+        for(MusicMatchEnum uriEnum:enums){
+            codeMap.put(uriEnum.code,uriEnum);
+        }
+    }
+
+    public MusicMatchEnum match(Uri uri){
+        final int code=uriMatcher.match(uri);
+        if(codeMap.get(code)==null){
+            throw new UnsupportedOperationException("Unknown uri:"+uri.toString()+" with code " + code);
+        }
+        return codeMap.get(code);
+    }
+
+    public String getType(Uri uri){
+        final int code=uriMatcher.match(uri);
+        if(codeMap.get(code)==null){
+            throw new UnsupportedOperationException("Unknown uri with code " + code);
+        }
+        return codeMap.get(code).contentType;
+    }
 }
