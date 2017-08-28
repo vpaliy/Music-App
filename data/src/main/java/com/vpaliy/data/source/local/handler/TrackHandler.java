333b951c2181725bb13402c5f1dc2d0b412ce4d9
vpaliy <vpaliy97@gmail.com>
2017-08-28 18:35:19 -0400

    data layer : : add user/track handlers

--- data/src/main/java/com/vpaliy/data/source/local/handler/TrackHandler.java
+++ data/src/main/java/com/vpaliy/data/source/local/handler/TrackHandler.java
@@ -1,8 +1,48 @@
 package com.vpaliy.data.source.local.handler;
 
-/**
- * Created by vpaliy on 28/08/17.
- */
+import android.content.ContentProvider;
+import android.database.Cursor;
 
+import com.vpaliy.data.source.local.MusicContract;
+import com.vpaliy.data.source.local.utils.DatabaseUtils;
+import com.vpaliy.domain.model.Track;
+
+import java.util.ArrayList;
+import java.util.List;
+import static com.vpaliy.data.source.local.MusicContract.Tracks;
+
+@SuppressWarnings({"unused","WeakerAccess"})
 public class TrackHandler {
+
+    private ContentProvider provider;
+
+    public TrackHandler(ContentProvider provider){
+        this.provider=provider;
+    }
+
+    public List<Track> queryAll(Query query){
+        if(query!=null){
+            Cursor cursor=provider.query(Tracks.CONTENT_URI,null,query.selection(),query.args(),null);
+            return queryAll(cursor);
+        }
+        return queryAll();
+    }
+
+    private List<Track> queryAll(Cursor cursor){
+        if(cursor!=null){
+            List<Track> tracks=new ArrayList<>();
+            while(cursor.moveToNext()){
+                Track track= DatabaseUtils.toTrack(cursor);
+                tracks.add(track);
+            }
+            if(!cursor.isClosed()) cursor.close();
+            return tracks;
+        }
+        return null;
+    }
+
+    public List<Track> queryAll(){
+        Cursor cursor=provider.query(Tracks.CONTENT_URI,null,null,null,null);
+        return queryAll(cursor);
+    }
 }
--- data/src/main/java/com/vpaliy/data/source/local/handler/UserHandler.java
+++ data/src/main/java/com/vpaliy/data/source/local/handler/UserHandler.java
@@ -1,4 +1,48 @@
 package com.vpaliy.data.source.local.handler;
 
+import android.content.ContentProvider;
+import android.database.Cursor;
+
+import com.vpaliy.data.source.local.utils.DatabaseUtils;
+import com.vpaliy.domain.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.vpaliy.data.source.local.MusicContract.Users;
+
+@SuppressWarnings({"unused","WeakerAccess"})
 public class UserHandler {
+
+    private ContentProvider provider;
+
+    public UserHandler(ContentProvider provider){
+        this.provider=provider;
+    }
+
+    public List<User> queryAll(Query query){
+        if(query!=null){
+            Cursor cursor=provider.query(Users.CONTENT_URI,null,query.selection(),query.args(),null);
+            return queryAll(cursor);
+        }
+        return queryAll();
+    }
+
+    private List<User> queryAll(){
+        Cursor cursor=provider.query(Users.CONTENT_URI,null,null,null,null);
+        return queryAll(cursor);
+    }
+
+    private List<User> queryAll(Cursor cursor){
+        if(cursor!=null){
+            List<User> users=new ArrayList<>(cursor.getCount());
+            while(cursor.moveToNext()){
+                User user= DatabaseUtils.toUser(cursor);
+                users.add(user);
+            }
+            if(!cursor.isClosed()) cursor.close();
+            return users;
+        }
+        return null;
+    }
 }
